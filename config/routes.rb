6ae47843e7b56d24dc3ffa015de334bbbc7ae6d5
tnksss <robsons@alunos.utfpr.edu.br>
2017-08-29 17:45:11 -0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'foo/bar'
+
+  get 'foo/baz'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-	root 'application#hello'
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+	root 'static_pages#home'
 end
