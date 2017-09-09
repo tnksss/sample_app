d2b85d29a850dfa0872c569dd526952f4d2226a3
tnksss <robsons@alunos.utfpr.edu.br>
2017-09-09 16:28:45 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
+  
 
-  get 'foo/bar'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 
-  get 'foo/baz'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-	root 'static_pages#home'
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | " . full_title
+  end
 end
