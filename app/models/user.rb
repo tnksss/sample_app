2a60e20be50d2cbb98c27fe4e7d685102f68ef8d
tnksss <robsons@alunos.utfpr.edu.br>
2017-11-15 11:29:42 -0200

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
     before_save     {email.downcase!}
     before_create   :create_activation_digest
     has_many        :microposts, dependent: :destroy
+    has_many        :active_relationships, class_name:  "Relationship",
+                                          foreign_key: "follower_id",
+                                            dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+    has_many :following, through: :active_relationships,  source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
 
 
     before_save { email.downcase! }
@@ -89,8 +98,26 @@ class User < ApplicationRecord
       reset_sent_at < 2.hours.ago
     end
     def feed
-      Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
     end
+    # Follows a user.
+    def follow(other_user)
+      following << other_user
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
+    end
+
 
     private
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  def setup 
-		@user = users(:michael)
-		@other_user = users(:archer)
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should redirect index when not logged in" do
@@ -52,5 +52,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
 
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
